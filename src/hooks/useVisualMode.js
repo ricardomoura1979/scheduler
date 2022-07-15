91bd2b095c319abde9b0bb8d2bc5e56bdbf43abf
ricardo <ricardomoura1979@gmail.com>
2022-07-15 19:21:22 -0400

    added initial, transition and back function

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -3,6 +3,29 @@ import { useState } from 'react';
 // initial mode setup
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
 
-  return { mode };
+  function transition(mode, replace = false) {          
+
+
+
+
+  }
+
+
+function back() {                
+
+
+
+
+   }
+
+
+
+
+
+
+  return { mode, transition, back };
 }
+
+
