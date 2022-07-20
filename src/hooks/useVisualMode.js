80ebeaecdaea80aabf97dd0d4edcc922ad798cc3
ricardo <ricardomoura1979@gmail.com>
2022-07-20 16:05:00 -0400

    commits updated

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -2,24 +2,27 @@ import { useState } from 'react';
 
 // initial mode setup
 export default function useVisualMode(initial) {
-  const [mode, setMode] = useState(initial);
+/*   const [mode, setMode] = useState(initial); */
   const [history, setHistory] = useState([initial]);
 
 
-
+// transition to a new mode
   function transition(mode, replace = false) {
-    setMode((prev) => mode);
-    setHistory((prev) => replace ? [...history, mode] : [...history, mode]);
+  /*   setMode((prev) => mode); */
+    setHistory((prev) => replace ? [...prev.slice(0, -1), mode] : [...history, mode]);
   };
 
+  // functionality to call back to the previous mode
   function back() {
-    setHistory((prev) => [...prev].pop(mode));
+    setHistory((prev) => prev.length > 1 ? prev.slice(0, -1) : prev)
+/*     setMode((prev) => [...history].pop(mode));
+  
     if (history.length > 1) {
       setMode((prev) => [...prev].pop(mode));
-    }
+    } */
   };
 
-  return { mode, transition, back };
+  return { mode: history[history.length -1], transition, back };
 }
 
 
